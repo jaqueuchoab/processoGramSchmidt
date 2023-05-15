0ba7566c59965dffc30f4243ab3371aa79a798d1
jaqueuchoab <jaqueline.uchoab@gmail.com>
2023-05-15 17:21:08 -0300

    fix: reorganizando a main

--- script/module/GramSchmidt.js
+++ script/module/GramSchmidt.js
@@ -2,10 +2,8 @@ import FerramentasAux from './FerramentasAux.js';
 
 export default class GramSchmidt extends FerramentasAux{
   calcularRazaoGramSchmidt(baseV, baseW) {
-    wAtual =
-      (this.calcularProdutoInterno(baseV, baseW)) /
-      (this.calcularProdutoInterno(baseW, baseW));
-    wDefinitivo = this.calcularProdutoVetorConstante(baseW, wAtual);
+    let wAtual = (this.calcularProdutoInterno(baseV, baseW)) / (this.calcularProdutoInterno(baseW, baseW));
+    let wDefinitivo = this.calcularProdutoVetorConstante(baseW, wAtual);
     return wDefinitivo;
   }
 
@@ -13,37 +11,49 @@ export default class GramSchmidt extends FerramentasAux{
     if (this.validarBaseOrtogonal(base) == false) {
       let novaBase = [];
       novaBase.push(base[0]);
-  
-      let wAtual;
+      let wAtual, wDefinitivo = [];
       let i = 1;
       while (i < base.length) {
-        wAtual = this.calcularProdutoInterno(base[i], novaBase[i - 1]);
-        wAtual = wAtual / (this.calcularProdutoInterno(novaBase[i - 1], novaBase[i - 1]));
-        wAtual = this.calcularProdutoVetorConstante(novaBase[i - 1], wAtual);
-        wAtual = this.calcularSubtracaoVetores(base[i], wAtual);
-  
-        novaBase.push(wAtual);
+        if (i == 1) {
+          wDefinitivo = this.calcularRazaoGramSchmidt(base[i], novaBase[i - 1]);
+          wDefinitivo = this.calcularSubtracaoVetores(base[i], wDefinitivo);
+          novaBase.push(wDefinitivo);
+        } else {
+          let k = 0;
+          while (k < i) {
+            wAtual = this.calcularRazaoGramSchmidt(base[i], novaBase[k]);
+            
+            if (k == 0) {
+              wDefinitivo = wAtual;
+              wDefinitivo = this.calcularSubtracaoVetores(base[i], wDefinitivo);
+            } else
+              wDefinitivo = this.calcularSubtracaoVetores(wDefinitivo, wAtual);
+            
+            k++;
+          }				
+          novaBase.push(wDefinitivo);
+        }
         i++;
       }
-  
       return novaBase;
-  
-    } else console.log('Base já é ortogonal, não há necessidade de ortogonalizar!');
+    } else
+      console.log(
+        "Base já é ortogonal, não há necessidade de ortogonalizar!"
+      );
   }
   
   calcularBaseOrtonormal(base) {
     if (this.validarBaseOrtonormal(base) == false) {
       let novaBase = [];
       let i = 0;
-      console.log(base.length);
-  
       do {
         novaBase.push(this.calcularNormalizacaoVetor(base[i]));
         i++;
       } while (i < base.length);
-  
       return novaBase;
-  
-    } else console.log('Base já é ortonormal, não há necessidade de ortogonalizar!');
+    } else
+      console.log(
+        "Base já é ortonormal, não há necessidade de ortogonalizar!"
+      );
   }
 }
